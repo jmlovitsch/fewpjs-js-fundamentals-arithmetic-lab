cf96618d0cf5aca629a89dade83b664e1bb1800f
john <jmlovitsch@gmail.com>
2020-11-01 10:02:14 -0600

    commit tree 5aa68c0c867e30039a25eaa72f023db0b40a9c84

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const numArray = (1, 2, 3, 20)
+const num1 = 1
+const num2 = 62
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = 8992 % 42
+
+let max = Math.max(numArray)
